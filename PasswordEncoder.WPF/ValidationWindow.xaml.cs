e92e0ea6017db978dd448a5a3697d88ebfc00212
mucnjakf <filip.mucnjak@outlook.com>
2024-02-13 16:01:07 +0100

    Initial

---
+++ PasswordEncoder.WPF/ValidationWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DesktopUI
+{
+    /// <summary>
+    /// Interaction logic for ValidationWindow.xaml
+    /// </summary>
+    public partial class ValidationWindow : Window
+    {
+        public ValidationWindow() => InitializeComponent();
+
+        private void BtnTryAgain_Click(object sender, RoutedEventArgs e) => Close();
+
+        private void SpToolBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                DragMove();
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,23 @@
+# PasswordEncoder
+
+Password Encoder Windows Desktop Application - 2018.
+
+---
+
+### **Overview**
+Password Encoder is a Windows desktop application for encrypting passwords using SHA256 hashing algorithm.
+SHA256 hashing algorithm computes a 256-bit digital fingerprint, whose hexadecimal writing consists of 64 characters.
+The algorithm uses non-linear function which means it is one-way and can not be decrypted.
+
+---
+
+### **Tech Stack**
+- .NET Framework 4.7.2 - WPF
+
+---
+
+### **Tools**
+- Source Control: JetBrains Space
+- IDE: Visual Studio 2019
+
+---
